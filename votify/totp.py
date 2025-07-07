26c6efcdcdb2e1f9545d11f4101eb58bebe8e657
Mateus <mnslmateus@gmail.com>
2025-07-07 18:00:17 -0300

    updating the totp secret

--- votify/totp.py
+++ votify/totp.py
@@ -6,8 +6,8 @@ import math
 class TOTP:
     def __init__(self) -> None:
         # dumped directly from the object, after all decryptions
-        self.secret = b"449443649084886328893534571041315"
-        self.version = 8
+        self.secret = b"10239356982684469120121471223494829410773366870"
+        self.version = 11
         self.period = 30
         self.digits = 6
 
