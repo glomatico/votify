2f832aa93e38965f5660b03a0358e92c55246839
Kyle Mohr <6644803+kylefmohr@users.noreply.github.com>
2025-08-03 18:57:26 -0500

    Update totp.py
    thanks to https://github.com/glomatico/votify/issues/56#issuecomment-3144473991

--- votify/totp.py
+++ votify/totp.py
@@ -6,8 +6,8 @@ import math
 class TOTP:
     def __init__(self) -> None:
         # dumped directly from the object, after all decryptions
-        self.secret = b"1231241278344110433411995110823210132101773562453811941121083082105117"
-        self.version = 21
+        self.secret = b'6934698199123836152873265447694113111117791033370'
+        self.version = 24
         self.period = 30
         self.digits = 6
 
