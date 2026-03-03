be1e1350dc7a2c583286133d120ca83ebd5afdb3
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2026-03-03 14:47:52 -0300

    Use 'str | None' annotations in downloader types

--- votify/downloader/types.py
+++ votify/downloader/types.py
@@ -8,8 +8,8 @@ from ..interface.types import SpotifyMedia
 class DownloadItem:
     media: SpotifyMedia
     uuid_: str = uuid.uuid4().hex[:8]
-    staged_path: str = None
-    final_path: str = None
-    playlist_file_path: str = None
-    synced_lyrics_path: str = None
-    cover_path: str = None
+    final_path: str | None = None
+    staged_path: str | None = None
+    playlist_file_path: str | None = None
+    synced_lyrics_path: str | None = None
+    cover_path: str | None = None
